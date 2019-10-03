1e514e127fcf5869ca94afc4e9610c5ec2aca144
Anshal <anshal570@gmail.com>
2019-10-03 17:43:37 +0530

    Added factorial program using recursion

--- recursion.java
+++ recursion.java
@@ -1,7 +1,7 @@
     class FactorialExample2{  
      static int factorial(int n){    
       if (n == 0)    
-        return 1;    
+        return 1;
       else    
         return(n * factorial(n-1));    
      }    
